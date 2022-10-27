a7bf9f7cb1be03fb211a2fd2b77918ce2a3a46ba
Vittorio Guerriero <vittorio.gue@gmail.com>
2022-10-27 14:36:49 +0100

    some abstract stuff

--- port/event_repository.py
+++ port/event_repository.py
@@ -1,9 +1,10 @@
 from domain.event import Event
+from abc import ABC, abstractmethod
 
 
-class EventRepository:
-    def write_mouse_event(self):
-        pass
+class EventRepository(ABC):
+    @abstractmethod
+    def write_mouse_event(self): pass
 
-    def get(self) -> list[Event]:
-        pass
+    @abstractmethod
+    def get(self) -> list[str]: pass
