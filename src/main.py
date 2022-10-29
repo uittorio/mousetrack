e7ad1217303e55ddd3794b448141dfa71b5cdd57
Vittorio Guerriero <vittorio.gue@gmail.com>
2022-10-29 18:28:27 +0100

    rename

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from time import sleep
 
 from adapters.file_event_repository import FileEventRepository
-from input_events.mouse_event_listener import mouse_event_listener
+from event_listeners.mouse_event_listener import mouse_event_listener
 from src.UI.ui import UI
-from src.input_events.keyboard_event_listener import keyboard_event_listener
+from src.event_listeners.keyboard_event_listener import keyboard_event_listener
 
 
 def main():
