f95b97436f015e58f6a5fe3e5420803390a6faf4
Vittorio Guerriero <vittorio.gue@gmail.com>
2022-11-06 06:08:10 +0000

    add all init files

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,7 @@
-
-from adapters.file_event_repository import FileEventRepository
-from event_listeners.mouse_event_listener import mouse_event_listener
 from src.UI.ui import UI
+from src.adapters.file_event_repository import FileEventRepository
 from src.domain.event_messages.event_messages import EventMessages
-from src.event_listeners.keyboard_event_listener import keyboard_event_listener
+from src.event_listeners.mouse_event_listener import mouse_event_listener
 
 
 def main():
@@ -14,6 +12,3 @@ def main():
     mouse_event_listener(event_messages)
     # keyboard_event_listener()
     ui.draw()
-
-
-main()
