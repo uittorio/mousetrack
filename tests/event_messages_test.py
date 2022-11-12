9be5ed4590ca3904e83ac0183e192c2ecf4815b6
Vittorio Guerriero <vittorio.gue@gmail.com>
2022-11-12 10:39:19 +0000

    add better tests

--- tests/event_messages_test.py
+++ tests/event_messages_test.py
@@ -4,6 +4,7 @@ from typing import Literal, Union
 import pytest as pytest
 
 from src.domain.event_messages.event_messages import EventMessages
+from src.domain.keyboard_event import KeyboardEvent
 from src.domain.mouse_event import MouseEvent
 from tests.fake.fake_event_repository import FakeEventRepository
 
@@ -26,7 +27,15 @@ def mouse_event(event_type: Union[Literal["click"], Literal["scroll"], Literal["
     }
 
 
-def test_adding_one_click_messages(fake_event_repository, event_messages):
+def keyboard_event(event_type: Union[Literal["keypressed"]], iso_date: str) -> KeyboardEvent:
+    iso_date = datetime.fromisoformat(iso_date)
+    return {
+        'time': datetime.timestamp(iso_date),
+        'type': event_type
+    }
+
+
+def test_adding_one_click_message(fake_event_repository, event_messages):
     event = mouse_event("click", "2022-10-30T06:25:11")
     event_messages.add_mouse_event(event)
     assert fake_event_repository.get_mouse_click_events() == [event]
@@ -42,7 +51,7 @@ def test_adding_two_click_messages(fake_event_repository, event_messages):
 def test_adding_one_move_message(fake_event_repository, event_messages):
     event = mouse_event("move", "2022-10-30T06:25:11")
     event_messages.add_mouse_event(event)
-    assert fake_event_repository.get_mouse_click_events() == [event]
+    assert fake_event_repository.get_mouse_move_events() == [event]
 
 
 def test_adding_two_move_message(fake_event_repository, event_messages):
@@ -50,7 +59,7 @@ def test_adding_two_move_message(fake_event_repository, event_messages):
     second_event = mouse_event("move", "2022-10-30T06:25:12")
     event_messages.add_mouse_event(first_event)
     event_messages.add_mouse_event(second_event)
-    assert fake_event_repository.get_mouse_click_events() == [first_event]
+    assert fake_event_repository.get_mouse_move_events() == [first_event]
 
 
 def test_adding_a_move_message_after_3_seconds_of_the_first_none(fake_event_repository, event_messages):
@@ -59,7 +68,7 @@ def test_adding_a_move_message_after_3_seconds_of_the_first_none(fake_event_repo
 
     event_messages.add_mouse_event(first_event)
     event_messages.add_mouse_event(second_event)
-    assert fake_event_repository.get_mouse_click_events() == [first_event, second_event]
+    assert fake_event_repository.get_mouse_move_events() == [first_event, second_event]
 
 
 def test_adding_a_move_message_after_2_seconds_of_the_first_two(fake_event_repository, event_messages):
@@ -69,7 +78,7 @@ def test_adding_a_move_message_after_2_seconds_of_the_first_two(fake_event_repos
     event_messages.add_mouse_event(first_event)
     event_messages.add_mouse_event(second_event)
     event_messages.add_mouse_event(third_event)
-    assert fake_event_repository.get_mouse_click_events() == [first_event]
+    assert fake_event_repository.get_mouse_move_events() == [first_event]
 
 
 def test_adding_a_move_message_after_4_seconds_of_the_first_one_on_the_next_minute(fake_event_repository,
@@ -79,4 +88,10 @@ def test_adding_a_move_message_after_4_seconds_of_the_first_one_on_the_next_minu
 
     event_messages.add_mouse_event(first_event)
     event_messages.add_mouse_event(second_event)
-    assert fake_event_repository.get_mouse_click_events() == [first_event, second_event]
+    assert fake_event_repository.get_mouse_move_events() == [first_event, second_event]
+
+
+def test_adding_one_keyboard_message(fake_event_repository, event_messages):
+    event = keyboard_event("keypressed", "2022-10-30T06:25:11")
+    event_messages.add_keyboard_event(event)
+    assert fake_event_repository.get_keyboard_events() == [event]
--- tests/fake/fake_event_repository.py
+++ tests/fake/fake_event_repository.py
@@ -1,24 +1,27 @@
+from src.domain.keyboard_event import KeyboardEvent
 from src.domain.mouse_event import MouseEvent
 from src.port.event_repository import EventRepository
 
 
 class FakeEventRepository(EventRepository):
-
     def __init__(self):
-        self.events = []
+        self.mouse_events = []
+        self.keyboard_events = []
 
     def get_mouse_scroll_events(self) -> list[MouseEvent]:
-        pass
+        return self.mouse_events
 
     def get_keyboard_events(self) -> list[MouseEvent]:
-        pass
+        return self.keyboard_events
 
     def get_mouse_move_events(self) -> list[MouseEvent]:
-        pass
+        return self.mouse_events
 
     def get_mouse_click_events(self) -> list[MouseEvent]:
-        return self.events
+        return self.mouse_events
 
     def add_mouse_event(self, event: MouseEvent):
-        self.events.append(event)
-        pass
+        self.mouse_events.append(event)
+
+    def add_keyboard_event(self, event: KeyboardEvent):
+        self.keyboard_events.append(event)
